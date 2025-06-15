10748b1c60e79aeb7d72e05a5ac98a00ebe8fbb8
Abhijeet2706 <96742355+Abhijeet2706@users.noreply.github.com>
2025-06-15 13:52:17 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Footer } from "@/components/footer";
-import Header from "@/components/header";
+import { Header } from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
